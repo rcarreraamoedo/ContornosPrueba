99f21a097ae762c782b157d2760d539216a89179
clagosuarez <clagosuarez@danielcastelao.org>
2019-10-23 13:36:55 +0200

    Cambio

--- contornosprueba/ContornosPrueba.java
+++ contornosprueba/ContornosPrueba.java
@@ -15,7 +15,7 @@ public class ContornosPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba!");
+        System.out.println("CAMBIO!");
     }
     
 }
